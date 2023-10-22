e8f637f77a1fda9a994727d2ab630611c1591b56
F-said <26397102+F-said@users.noreply.github.com>
2023-10-21 20:18:26 -0400

    Added folder

--- code/test/validate.py
+++ code/test/validate.py
@@ -19,6 +19,7 @@ while in the `code/test` folder to test the completeness of your code.
 The terminal output will tell you if your pipeline is successful.
 """
 import sys
+# TODO: new import
 import os
 
 from code.StockMetrics import StockMetrics
@@ -30,9 +31,9 @@ BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
 # Create a Metrics object
-print(os.path.isdir(os.getcwd() + r"\data"))
-print(os.listdir(os.getcwd()))
-metrics = StockMetrics(r"data\raw\amzn.csv")
+# TODO: new path
+file_path = os.path.join("data", "raw", "amzn.csv")
+metrics = StockMetrics(file_path)
 
 
 def pt1():
